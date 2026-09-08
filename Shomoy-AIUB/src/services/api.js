448911c472140e1ab891c3e2931a9aed38ebde06
Sudipta Sarker Unnayan <22-47656-2@student.aiub.edu>
2026-09-08 21:23:36 +0600

    modify

--- Shomoy-AIUB/src/services/api.js
+++ Shomoy-AIUB/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 // ⚠️ 'localhost' কাজ করবে না মোবাইল ডিভাইস/এমুলেটরে।
-// Windows-এ cmd খুলে `ipconfig` চালান, "IPv4 Address" (যেমন 192.168.0.105) কপি করুন।
-const BASE_URL = 'http://192.168.0.105:5000/api';
-
+// Windows-এ cmd খুলে `ipconfig` চালান, "IPv4 Address" (যেমন 192.168.0.109) কপি করুন।
+const BASE_URL = 'http://192.168.0.109:5000/api';
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
